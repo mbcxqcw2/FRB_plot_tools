18c5083ffc2a09a891e8b4b3c475aaf90f21e070
Walker <c.walker@cwalker-lt.skatelescope.org>
2019-06-21 10:45:10 +0100

    Added warning message to FRBcands_interactive.py, ensuring user pays attention to the DM sweep across the band for a candidate when attempting to click the candidate for a plot. If they are not zoomed in enough, the radius of the candidate marker will exceed the DM sweep time and they may miss the pulse.

--- FRBcands_interactive.py
+++ FRBcands_interactive.py
@@ -125,6 +125,7 @@ def func(event, ax,boxcar):
     #according to DM
     dt = (1./2.41e-16)*(((botchan*1e6)**(-2)-((topchan*1e6)**(-2))))*DM
     print '\nCalculated DM sweep across the band: {0}s'.format(dt)
+    print "\n\nWARNING: PAY ATTENTION TO THE DM SWEEP TIME ACROSS THE BAND!\nZOOM IN ENOUGH ON YOUR CANDIDATE PLOT THAT THE TIME WITHIN THE MARKER DOESN'T EXCWEED THIS TIME...\nOR ELSE YOU WILL MISS THE CANDIDATE WHEN PLOTTING!"
 
     #find nearest sample, sample time in fb file to the FRB
     nearest_sample = (np.abs(timesamples-time)).argmin()
